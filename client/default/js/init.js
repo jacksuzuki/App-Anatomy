0a4322d98e426d1e51a571c5ffba78f1648a27cf
syuszk+cloud@gmail.com <syuszk+cloud@gmail.com>
2012-03-25 23:04:59 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -16,6 +16,19 @@ $fh.ready(function () {
   );
   */
   
+    $fh.act({
+    act: "getAccessToken",
+    req: {}    
+    },
+  function(res){
+    console.log("success"+res);
+      $("#myimg").attr('src', res.photo);
+    },
+  function(msg, err){
+    console.log("error"+msg);
+     $("#myimg").attr('src',"hoge");
+    }
+  );
 
 });
 
